5a0144e839d644ba3c5dc4def23a105fa47c632e
Jay <mjzeel@gmail.com>
2017-10-09 23:19:09 +0530

    necessary module imports added

--- src/app/master-data/master-data.module.ts
+++ src/app/master-data/master-data.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule } 	from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { CompanyMasterModule } from './company-master/company-master.module';
+
 import { MasterDataComponent } from './master-data.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [MasterDataComponent]
+	imports: [
+		BrowserModule,
+		CommonModule,
+		BrowserAnimationsModule,
+		CompanyMasterModule
+	],
+	declarations: [
+		MasterDataComponent
+	]
 })
 export class MasterDataModule { }
