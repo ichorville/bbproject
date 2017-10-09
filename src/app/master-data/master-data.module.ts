1f271afc2b26ec1398f08aa99faa0ea89f60f9cf
Jay <mjzeel@gmail.com>
2017-10-09 21:33:52 +0530

    master-data module generated

---
+++ src/app/master-data/master-data.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MasterDataComponent } from './master-data.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MasterDataComponent]
+})
+export class MasterDataModule { }
