c12fda70c3ba73530511d8ed0fa6d9aba1038f47
Jay <mjzeel@gmail.com>
2017-10-09 23:19:45 +0530

    company-master-list component added

---
+++ src/app/master-data/company-master/company-master-list/company-master-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-company-master-list',
+  templateUrl: './company-master-list.component.html',
+  styleUrls: ['./company-master-list.component.css']
+})
+export class CompanyMasterListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
