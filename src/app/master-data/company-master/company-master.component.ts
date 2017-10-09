77e0b2a2f70d02d068c9c74dd8cd190107d548f2
Jay <mjzeel@gmail.com>
2017-10-09 21:33:21 +0530

    company-master module generated

---
+++ src/app/master-data/company-master/company-master.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-company-master',
+  templateUrl: './company-master.component.html',
+  styleUrls: ['./company-master.component.css']
+})
+export class CompanyMasterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
