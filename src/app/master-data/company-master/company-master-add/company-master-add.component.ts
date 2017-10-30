b2de4d2f2efca8718b8dfb367642bb89eee0b6e8
Jay <mjzeel@gmail.com>
2017-10-30 22:40:59 +0530

    add company logic created

--- src/app/master-data/company-master/company-master-add/company-master-add.component.ts
+++ src/app/master-data/company-master/company-master-add/company-master-add.component.ts
@@ -11,6 +11,8 @@ import { FormDropdown } from '../../../shared/form-elements/form-dropdown';
 import { FormSubmitEvent } from '../../../shared/custom-events/form-submit-event';
 import { FormSubmitCompleteEvent } from '../../../shared/custom-events/form-submit-complete-event';
 
+import { CompanyMasterService } from '../company-master.service';
+
 import { LoginVariable } from '../../../global';
 
 @Component({
@@ -27,7 +29,8 @@ export class CompanyMasterAddComponent implements OnInit {
 	formElements: FormElement<any>[];
 
 	constructor(
-		private router: Router
+		private router: Router,
+		private _cms: CompanyMasterService
 	) { 
 		this.title = 'Add Outlet';
 		this.buttonValue = 'Save';
@@ -41,19 +44,72 @@ export class CompanyMasterAddComponent implements OnInit {
 	}
 
 	submit(formSubmitEvent: FormSubmitEvent) {
-
+		let formValues = formSubmitEvent.formObject;
+		let country: any = {
+			'id': formValues.id,
+			'name': formValues.name,
+			'address': formValues.address,
+			'phone': formValues.phone,
+			'email': formValues.email,
+			'status': false
+		};
+		this._cms.create(country).then(() => {
+			this.router.navigateByUrl(`/master-data/companies`);
+		});
 	}
 
 	private createForm() {
 		this.formElements = [
 			new FormTextbox({
 				key: 'id',
-				label: 'Id',
+				label: 'Company Code',
 				value: '',
 				controlType: 'textbox',
 				required: true,
 				order: 1,
-				placeholder: 'Id',
+				placeholder: 'Company Code',
+				validators: [
+					Validators.required
+				]
+			}),
+			new FormTextbox({
+				key: 'name',
+				label: 'Company Name',
+				value: '',
+				controlType: 'textbox',
+				required: true,
+				order: 2,
+				placeholder: 'Company Name'
+			}),
+			new FormTextbox({
+				key: 'address',
+				label: 'Address',
+				value: '',
+				controlType: 'textbox',
+				required: true,
+				order: 3,
+				placeholder: 'Address'
+			}),
+			new FormTextbox({
+				key: 'phone',
+				label: 'Phone',
+				value: '',
+				controlType: 'textbox',
+				required: true,
+				order: 4,
+				placeholder: 'Phone',
+				validators: [
+					Validators.required
+				]
+			}),
+			new FormTextbox({
+				key: 'email',
+				label: 'Email',
+				value: '',
+				controlType: 'textbox',
+				required: true,
+				order: 5,
+				placeholder: 'Email',
 				validators: [
 					Validators.required
 				]
