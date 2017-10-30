729b7652d9bb76e87b9ee6b480e1db82be12e4f4
Jay Connor <mjzeel@gmail.com>
2017-10-30 17:19:22 +0530

    dynamic form module added and changed md tage to mat tags

---
+++ src/app/shared/dynamic-form/dynamic-form.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DynamicFormComponent } from './dynamic-form.component';
+
+import { MaterialModule } from '../material/material.module';
+
+@NgModule({
+	imports: [
+		BrowserModule,
+		CommonModule,
+		FormsModule,
+		ReactiveFormsModule,
+		MaterialModule
+	],
+	declarations: [
+		DynamicFormComponent
+	],
+	exports: [
+		DynamicFormComponent
+	]
+})
+
+export class DynamicFormModule {}
